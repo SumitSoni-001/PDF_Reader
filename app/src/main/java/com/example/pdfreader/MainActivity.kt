dde96c44de6e82f1fab7dbfd6ad8d0cc8bb2ed61
SumitSoni-001 <geekysumit24680@gmail.com>
2022-04-17 19:33:15 +0530

    Persistent bottom sheet added

--- app/src/main/java/com/example/pdfreader/MainActivity.kt
+++ app/src/main/java/com/example/pdfreader/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.pdfreader
 
 import android.os.AsyncTask
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import com.example.pdfreader.databinding.ActivityMainBinding
 import com.github.barteksc.pdfviewer.PDFView
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.persistent_bottom_sheet.*
 import java.io.BufferedInputStream
 import java.io.InputStream
 import java.net.HttpURLConnection
@@ -16,6 +21,7 @@ lateinit var pdfView: PDFView
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,21 +29,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         pdfView = findViewById(R.id.pdfView)
+
+//        val bottomSheet = findViewById<LinearLayout>(R.id.bottomSheet)
+        bottomSheetBehavior = BottomSheetBehavior.from(persistentBottomSheet)
+
         binding.openBtn.isVisible = true
 
         binding.openBtn.setOnClickListener {
+            Toast.makeText(this, "Persistent Bottom Sheet", Toast.LENGTH_SHORT).show()
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
 
+        fromAsset.setOnClickListener {
             binding.openBtn.isVisible = false
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+
+            /** PDF from Asset */
+            pdfView.fromAsset("collection.pdf")
+//                    .enableSwipe(true)
+//                    .enableDoubletap(true)
+                .load()
+        }
+
+        fromUrl.setOnClickListener {
+            binding.openBtn.isVisible = false
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+            Toast.makeText(this, "Wait...", Toast.LENGTH_LONG).show()
 
             /** PDF from URI */
             getPdf().execute()
-
-            /** PDF from Asset */
-//            pdfView.fromAsset("collection.pdf")
-//                .enableSwipe(true)
-//                .enableDoubletap(true)
-//                .load()
         }
+
     }
 
 }
